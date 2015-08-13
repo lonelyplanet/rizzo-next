72a45dc0aa16e5d53533ea2f058287847f6edb0c
Joe Shepherd <joeshepmedia@gmail.com>
2015-08-13 11:28:48 -0500

    Add dynamic map trigger logic into static map

--- src/components/map_static/index.js
+++ src/components/map_static/index.js
@@ -1,1 +1,15 @@
 require("./_map_static.scss");
+
+let mapLoaded = false;
+$(".js-open-map").on("click", function() {
+  if (!mapLoaded) {
+    require([
+      "../map/map_component"
+    ], (MapComponent) => {
+      let map = new MapComponent({
+        el: ".map_holder"
+      });
+      map.open();
+    });
+  }
+});
