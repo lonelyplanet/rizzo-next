85a08261377b16f32d746b8286673d78509462fc
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2015-09-11 09:18:08 -0500

    Add an event when users search for hotels

--- src/components/hotels/hotels_widget.js
+++ src/components/hotels/hotels_widget.js
@@ -1,54 +1,61 @@
 import { Component } from "../../core/bane";
+import assign from "lodash/object/assign";
 import "pickadate/lib/picker.date";
-import $ from "jquery";
+
+const dateDefaults = {
+  format: "mm/d/yyyy",
+  formatSubmit: "dd/mm/yyyy",
+  hiddenName: true,
+  labelMonthNext: "Go to the next month",
+  labelMonthPrev: "Go to the previous month",
+  labelMonthSelect: "Pick a month from the dropdown",
+  labelYearSelect: "Pick a year from the dropdown"
+};
 
 class HotelsWidget extends Component {
   initialize() {
+    this.events = {
+      "submit #hotel-search-form": "searchHotels"
+    };
+
     let dates = this.$el.find("input[type='date']"),
-        checkInDate = dates[0],
-        checkOutDate = dates[1],
-        today = new Date(),
-        startDate = $(checkInDate),
-        endDate = $(checkOutDate),
-        nextDate = (date) => {
-          let tmpDate = new Date(date);
-          tmpDate.setDate(date.getDate() + 1);
-          return tmpDate;
-        },
-        changeDate = () => {
-          let existingEndDate = new Date(endDate.pickadate("picker").get()),
-                newStartDate = new Date(startDate.pickadate("picker").get());
-           if (existingEndDate.toString() === "Invalid Date" || newStartDate > existingEndDate) {
-              let newMinimumEndDate = new Date(newStartDate.getTime() + 24 * 60 * 60 * 1000);
-              endDate.pickadate("picker").set({
-                "min": newMinimumEndDate,
-                "select": newMinimumEndDate});
-            }
-        };
+        startDate = $(dates[0]),
+        endDate = $(dates[1]),
+        today = new Date();
 
-    startDate.pickadate({
+    startDate.pickadate(assign({
       min: today,
-      format: "mm/d/yyyy",
-      formatSubmit: "dd/mm/yyyy",
-      hiddenName: true,
-      labelMonthNext: "Go to the next month",
-      labelMonthPrev: "Go to the previous month",
-      labelMonthSelect: "Pick a month from the dropdown",
-      labelYearSelect: "Pick a year from the dropdown"
-    });
+    }, dateDefaults));
 
-    endDate.pickadate({
-      min: nextDate(today),
-      format: "mm/d/yyyy",
-      formatSubmit: "dd/mm/yyyy",
-      hiddenName: true,
-      labelMonthNext: "Go to the next month",
-      labelMonthPrev: "Go to the previous month",
-      labelMonthSelect: "Pick a month from the dropdown",
-      labelYearSelect: "Pick a year from the dropdown"
-    });
+    endDate.pickadate(assign({
+      min: this.nextDate(today)
+    }, dateDefaults));
 
-    startDate.change(changeDate.bind(this));
+    startDate.change(() => this.changeDate(endDate, startDate));
+  }
+  nextDate(date) {
+    let tmpDate = new Date(date);
+    tmpDate.setDate(date.getDate() + 1);
+    return tmpDate;
+  }
+  changeDate (endDate, startDate){
+    let existingEndDate = new Date(endDate.pickadate("picker").get()),
+        newStartDate = new Date(startDate.pickadate("picker").get());
+   
+    if (existingEndDate.toString() === "Invalid Date" || newStartDate > existingEndDate) {
+      let newMinimumEndDate = new Date(newStartDate.getTime() + 24 * 60 * 60 * 1000);
+      endDate.pickadate("picker").set({
+        "min": newMinimumEndDate,
+        "select": newMinimumEndDate});
+    }
+  }
+  searchHotels(event) {
+    let serialized = $(event.target).serialize();
+
+    window.lp.analytics.api.trackEvent({ 
+      category: "Partner Click",
+      action: `partner=booking&${serialized}`
+    });
   }
 }
 
