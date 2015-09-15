2e47fbab20f51e36772b011cd46f276b7de07014
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2015-09-15 15:45:21 -0500

    Add in logging with Airbrake

--- src/components/sub_nav/index.js
+++ src/components/sub_nav/index.js
@@ -1,80 +1,84 @@
 require("./_sub_nav.scss");
 
-let debounce = require("lodash/function/debounce"),
-    $subNav = $(".js-sub-nav");
+export default class SubNav {
+  constructor() {
+    let debounce = require("lodash/function/debounce"),
+        $subNav = $(".js-sub-nav");
 
-if ($subNav.length) {
-  let subNavTop = $subNav.offset().top,
-      firstTrigger = true,
-      $window = $(window),
-      fixedState, fixedSubNav;
+    if ($subNav.length) {
+      let subNavTop = $subNav.offset().top,
+          firstTrigger = true,
+          $window = $(window),
+          fixedState, fixedSubNav;
 
-  $(document).on("click", ".js-sub-nav-lnk", function(e) {
-    let target = this.hash;
-    let $target = $(target);
-    let navHeight = $subNav.height();
+      $(document).on("click", ".js-sub-nav-lnk", function(e) {
+        let target = this.hash;
+        let $target = $(target);
+        let navHeight = $subNav.height();
+        
+        e.preventDefault();
 
-    e.preventDefault();
+        if($target.parents(".segment").length > 0) {
+          $target = $target.parents(".segment");
+        }
 
-    if($target.parents(".segment").length > 0) {
-      $target = $target.parents(".segment");
-    }
-
-    $("html, body").stop().animate({
-      scrollTop: $target.offset().top - navHeight
-    }, 500, "swing", () => {
-      window.location.hash = target;
-    });
-  });
-
-  if (window.location.hash) {
-    $subNav.find(`[href="${window.location.hash}"]`).trigger("click");
-  }
-
-  let $links = $(".js-sub-nav-lnk"),
-      $components = $links.map((i, el) => {
-        return document.getElementById(el.href.split("#")[1]);
+        $("html, body").stop().animate({
+          scrollTop: $target.offset().top - navHeight
+        }, 500, "swing", () => {
+          window.location.hash = target;
+        });
       });
-  let $last;
 
-  $window.scroll(debounce(() => {
-    if (firstTrigger) {
-      fixedSubNav = $subNav
-        .clone(true)
-        .addClass("sub-nav--fixed");
-
-      firstTrigger = false;
-    }
-
-    if ($window.scrollTop() > subNavTop){
-      if(!fixedState){
-        fixedSubNav.appendTo( "body" );
-        fixedState = true;
+      if (window.location.hash) {
+        $subNav.find(`[href="${window.location.hash}"]`).trigger("click");
       }
-    } else if (fixedState) {
-        fixedSubNav.detach();
-        fixedState = false;
+
+      let $links = $(".js-sub-nav-lnk"),
+          $components = $links.map((i, el) => {
+            return document.getElementById(el.href.split("#")[1]);
+          });
+      let $last;
+
+      $window.scroll(debounce(() => {
+        if (firstTrigger) {
+          fixedSubNav = $subNav
+            .clone(true)
+            .addClass("sub-nav--fixed");
+
+          firstTrigger = false;
+        }
+
+        if ($window.scrollTop() > subNavTop){
+          if(!fixedState){
+            fixedSubNav.appendTo( "body" );
+            fixedState = true;
+          }
+        } else if (fixedState) {
+            fixedSubNav.detach();
+            fixedState = false;
+        }
+
+        let scrollTop = $(window).scrollTop() + $subNav.height();
+
+        let $current = $components.map((i, el) => {
+          if (el.offsetTop < scrollTop) {
+            return el;
+          }
+        });
+
+        if ($current.length) {
+          fixedSubNav.find("a").removeClass("sub-nav__link--active");
+
+          fixedSubNav
+            .find(`a[href*="#${$current[$current.length - 1].id}"]`)
+              .addClass("sub-nav__link--active");
+        }
+
+      }, 10));
+
+      $window.resize(debounce(() => {
+        subNavTop = $subNav.offset().top;
+      }, 10));
     }
-
-    let scrollTop = $(window).scrollTop() + $subNav.height();
-
-    let $current = $components.map((i, el) => {
-      if (el.offsetTop < scrollTop) {
-        return el;
-      }
-    });
-
-    if ($current.length) {
-      fixedSubNav.find("a").removeClass("sub-nav__link--active");
-
-      fixedSubNav
-        .find(`a[href*="#${$current[$current.length - 1].id}"]`)
-          .addClass("sub-nav__link--active");
-    }
-
-  }, 10));
-
-  $window.resize(debounce(() => {
-    subNavTop = $subNav.offset().top;
-  }, 10));
+  }
 }
---
+++ src/core/logger.js
@@ -0,0 +1,20 @@
+import airbrakeJs from "airbrake-js";
+
+let airbrake = new airbrakeJs({
+  projectId: 108616, 
+  projectKey: "046ae0791af77310d8cfe001786fad6f"
+});
+
+export default class Logger {
+  /**
+   * Log an error
+   * @param {Error|Object|String} message Either string or object containing error details
+   */
+  error(err) {
+    console.error(err);
+
+    if (ENV_PROD) {
+      airbrake.notify(err);
+    }
+  }
+}
