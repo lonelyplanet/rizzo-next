7368fe66504746d2c49998f2a3fad455224d965e
Joe Shepherd <joeshepmedia@gmail.com>
2015-11-19 10:33:52 -0600

    Add hide/close; multiple background options

--- src/components/alert/index.js
+++ src/components/alert/index.js
@@ -1,1 +1,27 @@
+import Component from "../../core/component";
+import waitForTransition from "../../core/utils/waitForTransition";
+// import $ from "jquery";
 require("./_alert.scss");
+
+class Alert extends Component {
+  initialize() {
+    this.events = {
+      "click .js-close": (e) => {
+        e.preventDefault();
+        console.log("close click");
+        this.hide();
+      }
+    }
+  }
+
+  hide() {
+    this.$el.removeClass("alert--is-visible");
+
+    return waitForTransition(this.$el, { fallbackTime: 1000 })
+      .then(() => {
+        this.$el.detach();
+      });
+  }
+}
+
+export default Alert;
