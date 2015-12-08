a9caf6a2e2fc90238b581641790eb5d386c93a83
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2015-12-08 13:29:05 -0600

    Going straight through utag for DN

--- src/core/events/trackers/analytics.js
+++ src/core/events/trackers/analytics.js
@@ -1,4 +1,5 @@
 import isJson from "../../utils/is_json";
+import isDev from "../../utils/is_dev";
 
 /** 
  * Track an event with our analytics library
@@ -6,26 +7,24 @@ import isJson from "../../utils/is_json";
  */
 export default function({ name, data } = {}) {
   /* global utag */
-  if (utag && typeof utag.link === "function") {
-    data = (isJson(data) ? JSON.parse(data) : data) || {};
+  data = (isJson(data) ? JSON.parse(data) : data) || {};
     
-    let category, action;
+  let category, action;
 
-    if (name.toLowerCase() === "partner search") {
-        category = "Partner";
-        action = "Search";
-    }
-
-    let event = {
-        ga_event_category: category || "Destinations Next",
-        ga_event_action: action || name,
-        ga_event_label: JSON.stringify(data)
-    };
-
-    if (typeof ENV_PROD !== "undefined" && !ENV_PROD) {
-      console.log(`utag: ${JSON.stringify(event)}`);
-    } else {
-      utag.link(event);      
-    }
+  if (name.toLowerCase() === "partner search") {
+      category = "Partner";
+      action = "Search";
   }
+
+  let event = {
+    ga_event_category: category || "Destinations Next",
+    ga_event_action: action || name,
+    ga_event_label: JSON.stringify(data)
+  };
+
+  if (isDev()) {
+    console.log(`utag: ${JSON.stringify(event)}`);
+  } else if (utag && typeof utag.link === "function") {
+    utag.link(event);
+  } 
 };
