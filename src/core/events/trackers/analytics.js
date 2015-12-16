d17fcec67a019a1c44746765b825f09c3547245e
Tom Cunningham <thomasthesecond@gmail.com>
2015-12-16 15:03:27 -0600

    Update analytics.js to better track events
    Category should be something specific, i.e., what is it that we're tracking? Example: article-gallery
    Event should be an event, i.e., click, scroll, hover, etc.
    Label should be a string and describe what is being tracked, i.e., pathname for article loaded via ajax
    
    This will probably need to be refactored so that we're not passing an object to label and so we don't need to use condtionals.

--- src/core/events/trackers/analytics.js
+++ src/core/events/trackers/analytics.js
@@ -9,23 +9,45 @@ export default function({ name, data } = {}) {
   /* global utag */
   data = (isJson(data) ? JSON.parse(data) : data) || {};
 
-  let category, action;
+  let category, action, label;
 
   if (name.toLowerCase() === "partner search") {
     category = "Partner";
     action = "Search";
-  } else if (name.toLowerCase() === "ad page load impression") {
+  } else if (name.toLowerCase() === "article ad impression load") {
     category = "advertising";
     action = "page-load-impression";
-  } else if (name.toLowerCase() === "ad ajax load impression") {
+    label = data;
+  } else if (name.toLowerCase() === "article ad impression scroll") {
     category = "advertising";
-    action = "ajax-page-load-impression";
+    action = "page-load-impression";
+    label = data;
+  } else if (name.toLowerCase() === "article pageview scroll") {
+    category = "related-article";
+    action = "scroll";
+    label = data;
+  } else if (name.toLowerCase() === "article pageview click") {
+    category = "related-article";
+    action = "click";
+    label = data;
+  } else if (name.toLowerCase() === "gallery click") {
+    category = data.category;
+    action = "click";
+    label = data.label;
+  } else if (name.toLowerCase() === "share menu click") {
+    category = data.category;
+    action = "click";
+    label = data.label;
+  } else if (name.toLowerCase() === "share button click") {
+    category = data.category;
+    action = "click";
+    label = data.label;
   }
 
   let event = {
     ga_event_category: category || "Destinations Next",
     ga_event_action: action || name,
-    ga_event_label: JSON.stringify(data)
+    ga_event_label: label || JSON.stringify(data)
   };
 
   if (isDev()) {
