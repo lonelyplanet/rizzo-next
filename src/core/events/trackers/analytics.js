5a6211cef46c26f180dc6656193735b4329fa202
Tom Cunningham <thomasthesecond@gmail.com>
2015-12-17 11:18:20 -0600

    Update analytics
    - Refactor analytics.js to use GA event mapping
    - Create GA event map

--- src/core/events/trackers/analytics.js
+++ src/core/events/trackers/analytics.js
@@ -1,5 +1,6 @@
 import isJson from "../../utils/is_json";
 import isDev from "../../utils/is_dev";
+import gaEventMap from "./ga_event_map";
 
 /**
  * Track an event with our analytics library
@@ -9,50 +10,27 @@ export default function({ name, data } = {}) {
   /* global utag */
   data = (isJson(data) ? JSON.parse(data) : data) || {};
 
-  let category, action, label;
+  let mappedEvent,
+      gaEventData = {
+        category: "Destinations Next",
+        action: name,
+        label: isJson(data) ? JSON.stringify(data) : data
+      };
 
-  if (name.toLowerCase() === "partner search") {
-    category = "Partner";
-    action = "Search";
-  } else if (name.toLowerCase() === "article ad impression load") {
-    category = "advertising";
-    action = "page-load-impression";
-    label = data;
-  } else if (name.toLowerCase() === "article ad impression scroll") {
-    category = "advertising";
-    action = "page-load-impression";
-    label = data;
-  } else if (name.toLowerCase() === "article pageview scroll") {
-    category = "related-article";
-    action = "scroll";
-    label = data;
-  } else if (name.toLowerCase() === "article pageview click") {
-    category = "related-article";
-    action = "click";
-    label = data;
-  } else if (name.toLowerCase() === "gallery click") {
-    category = data.category;
-    action = "click";
-    label = data.label;
-  } else if (name.toLowerCase() === "share menu click") {
-    category = data.category;
-    action = "click";
-    label = data.label;
-  } else if (name.toLowerCase() === "share button click") {
-    category = data.category;
-    action = "click";
-    label = data.label;
+  if (mappedEvent = gaEventMap[name]) {
+    for (let name in mappedEvent) {
+      gaEventData[name] = mappedEvent[name];
+    }
   }
 
-  let event = {
-    ga_event_category: category || "Destinations Next",
-    ga_event_action: action || name,
-    ga_event_label: label || JSON.stringify(data)
-  };
+  let utagEvent = Object.keys(gaEventData).reduce((memo, key) => {
+    memo["ga_event_" + key] = mappedEvent[key] || gaEventData[key];
+    return memo;
+  }, {});
 
   if (isDev()) {
-    console.log(`utag: ${JSON.stringify(event)}`);
+    console.log(`utag: ${JSON.stringify(utagEvent)}`);
   } else if (utag && typeof utag.link === "function") {
-    utag.link(event);
+    utag.link(utagEvent);
   }
 };
