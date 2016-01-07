2f42f687a755abbd3a4e350f188c0925a4060a2c
Tom Cunningham <thomasthesecond@gmail.com>
2016-01-07 13:47:03 -0600

    Update sights heading

--- src/components/sights/sights_component.js
+++ src/components/sights/sights_component.js
@@ -5,8 +5,9 @@ export default class Sights extends Component {
   initialize() {
     this.subscribe();
   }
-  @subscribe("ttd.removed", "components")
+
+  @subscribe("ttd.removed", "components");
   _changeTitle() {
-    this.$el.find(".sights__heading").toggleClass("is-hidden");
+    this.$el.find(".js-sights-heading").toggleClass("sights__heading--large");
   }
 }
