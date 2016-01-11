dae006a8de559948af5a77a4e5df6c1f3a553dba
Tom Cunningham <thomasthesecond@gmail.com>
2016-01-11 12:24:50 -0600

    Refactor tabs
    Adds classnames to `li` and `a`, as well as `js-` classes. Update spec.

--- spec/unit/components/tabs.spec.js
+++ spec/unit/components/tabs.spec.js
@@ -6,13 +6,13 @@ let expect = require("expect.js");
 let fixture = `
 <div class="tabs">
   <ul class="tabs__links">
-    <li><a href="#tab1">Tab 1</a></li>
-    <li><a href="#tab2">Tab 2</a></li>
+    <li class="tabs__item"><a class="tabs__link js-tabs-link" href="#tab1">Tab 1</a></li>
+    <li class="tabs__item"><a class="tabs__link js-tabs-link" href="#tab2">Tab 2</a></li>
   </ul>
-  <div class="tabs__content" id="tab1">
+  <div class="tabs__content js-tabs-content" id="tab1">
     Tab 1 Content
   </div>
-  <div class="tabs__content" id="tab2">
+  <div class="tabs__content js-tabs-content" id="tab2">
     Tab 2 Content
   </div>
 </div>
@@ -41,7 +41,7 @@ describe("tabs component", () => {
 
     it("should set tabs as active when clicking a link", function() {
       $el.find("[href='#tab2']").click();
-      
+
       expect($el.find("[href='#tab1']").hasClass("is-active")).to.not.be.ok();
       expect(tabs.active.$tab[0].id).to.be("tab2");
     });
