94962f2f4f0c2875e29b75f0a4500d0a7ee9b3a1
Paweł Jasiołek <pawel.jasiolek@gmail.com>
2016-01-27 17:16:55 +0100

    Scroll to component

---
+++ src/components/scroll_to/scroll-to.js
@@ -0,0 +1,26 @@
+import { Component } from "../../core/bane";
+import $ from "jquery";
+import Scroll from "scroll-js";
+
+class ScrollToComponent extends Component {
+  initialize() {
+    this.isSocialShareMenuHidden = false;
+
+    this.events = {
+      "click .js-action-scroll-to": "scrollLinkClicked"
+    };
+
+    this._scroll = new Scroll({
+        el: document.body
+    });
+  }
+
+  scrollLinkClicked(event) {
+    event.preventDefault();
+
+    let target = document.getElementById(event.currentTarget.hash.substr(1));
+    this._scroll.toElement(target, {duration: 1500});
+  }
+}
+
+export default ScrollToComponent;
