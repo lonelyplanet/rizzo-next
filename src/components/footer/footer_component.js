69a9a9d6b91ea8a0c3b1603f18e9e468a6069a21
laserchicken <laserchicken71@gmail.com>
2017-01-13 17:41:32 +0100

    switch footer newlsetter to gonzo endpoint

--- src/components/footer/footer_component.js
+++ src/components/footer/footer_component.js
@@ -4,6 +4,10 @@ import $ from "jquery";
 class Footer extends Component {
   initialize() {
     this.updateLocationOnChange();
+    this.$form = $(".js-newsletter-form");
+    this.$submitButton = $(".js-submit");
+    this.$success = $(".js-success");
+    this.$submitButton.on("click", this.submit.bind(this));
   }
 
   updateLocationOnChange() {
@@ -15,6 +19,27 @@ class Footer extends Component {
       event.preventDefault();
     });
   }
+
+  handleSubmitSuccess() {
+    this.$form.addClass("is-hidden");
+    this.$success.removeClass("is-hidden");
+  }
+
+  submit(event) {
+    event.preventDefault();
+    $.post(this.$form.attr("action"), this.$form.serialize())
+      .done(() => {
+        this.handleSubmitSuccess();
+      })
+      .fail((xhr) => {
+        if (xhr.status === 409) {
+          this.handleSubmitSuccess();
+        } else {
+          console.log("error");
+        }
+      });
+  }
+
 }
 
 export default Footer;
