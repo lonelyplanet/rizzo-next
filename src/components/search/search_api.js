c7b4fc9a9d9bdee0eba79e245951e040e1acc091
Chad Harter <chad.harter@budgettravel.com>
2017-05-01 12:49:44 -0400

    Updated video search endpoint to production url

--- src/components/search/search_api.js
+++ src/components/search/search_api.js
@@ -1,6 +1,37 @@
 import $ from "jquery";
 import SearchServerActions from "./search_server_actions";
 
+const mergeResults = (query, searchResults, videoResults) => {
+
+  let bestVideoResults = [];
+  if (query.length > 1) {
+    bestVideoResults = videoResults.filter((v) => {
+      return v.name.toLowerCase().startsWith(query.toLowerCase());
+    });
+  }
+
+  const bestVideoResultCount = bestVideoResults.length;
+
+  // Merge results by alternating between the best video results and the search results
+  const results = [];
+
+  while (bestVideoResults.length || searchResults.length) {
+    if (bestVideoResults.length) {
+      results.push(bestVideoResults.shift());
+    }
+    if (searchResults.length) {
+      results.push(searchResults.shift());
+    }
+  }
+
+  // Make sure at least 1 video result appears in the top 5
+  if (!bestVideoResultCount && videoResults.length) {
+    results.splice(4, 0, videoResults.shift());
+  }
+
+  return results;
+};
+
 let SearchApi = {
   search: (query) => {
     let searchResults = null;
@@ -16,7 +47,7 @@ let SearchApi = {
     });
 
     $.ajax({
-      url: `http://localhost:3000/video/search.json?q=${query}`
+      url: `https://www.lonelyplanet.com/video/search.json?q=${query}`
     }).done((results) => {
       videoResults = results;
       if (searchResults) {
@@ -26,48 +57,5 @@ let SearchApi = {
   }
 };
 
-const mergeResults = (query, searchResults, videoResults) => {
-  let results = [];
-
-  let bestVideoResults = [];
-
-  // Best video results are results that start with the search query
-  if (query.length > 1) {
-    bestVideoResults = videoResults.filter((v) => {
-      return v.name.toLowerCase().startsWith(query.toLowerCase());
-    });
-  }
-
-  // Second best video results are results where one word within the
-  // video name starts with the search query
-  videoResults = videoResults.filter((v) => {
-    return (
-      (bestVideoResults.findIndex((x) => x.name === v.name) === -1) &&
-      (query.indexOf(" ") === -1 && !!(v.name.toLowerCase().split(" ").find((word) => word.startsWith(query.toLowerCase()))))
-      );
-  });
-
-  const bestVideoResultCount = bestVideoResults.length;
-
-  // Merge results by alternating between the best video results and the search results
-  while (bestVideoResults.length || searchResults.length) {
-    if (bestVideoResults.length) {
-      results.push(bestVideoResults.shift());
-    }
-    if (searchResults.length) {
-      results.push(searchResults.shift());
-    }
-  }
-
-  // Make sure at least 1 video result appears in the top 5
-  if (!bestVideoResultCount && videoResults.length) {
-    results.splice(4, 0, videoResults.shift());
-  }
-
-  // Add less relevant video results to the end
-  results = results.concat(videoResults);
-
-  return results;
-};
 
 export default SearchApi;
