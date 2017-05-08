e86012db450fb97a0188edfbc1c63ba3376d56c2
Chad Harter <harterc1@live.com>
2017-05-08 09:56:37 -0400

    Video component can now query playlists from brightcove (#647)

--- src/components/video/brightcove.js
+++ src/components/video/brightcove.js
@@ -6,6 +6,11 @@ class Brightcove extends VideoPlayer {
 
   initialize(options) {
     super.initialize(options);
+
+    this.autoplay = false;
+    this.videos = [];
+    this.currentVideoIndex = null;
+
     this.events["click .video-js"] = "onClickVideo";
   }
 
@@ -17,98 +22,113 @@ class Brightcove extends VideoPlayer {
   }
 
   onClickVideo(event) {
-    // Prevent event from bubbling into the UI 
+    // Prevent event from bubbling into the UI
     // when the user interacts with the video.
     event.stopPropagation();
   }
 
   play() {
     super.play();
+    this.autoplay = true;
     this.player.play();
   }
 
   pause() {
     super.pause();
+    this.autoplay = false;
     this.player.pause();
+  }
+
+  start() {
+    this.currentVideoIndex = null;
+    this.loadNextVideo();
+    this.play();
   }
 
   setup() {
     let self = this;
     videojs(this.videoEl).ready(function () {
       self.player = this;
-      self.player.on("ended", () => { self.trigger("ended"); });
-      self.setInitialDimensions();
+      self.player.on("loadstart", self.onPlayerLoadStart.bind(self));
+      self.player.on("ended", self.onPlayerEnded.bind(self));
       self.trigger("ready");
     });
   }
 
-  search() {
-    try {
-      let videoId = "ref:dest_" + window.lp.place.atlasId;
-      return Promise.resolve([videoId]);
-    }
-    catch (e) {
-      return Promise.resolve([]);
+  onPlayerLoadStart() {
+    this.renderSEOMarkup();
+    this.configureOverlays();
+
+    if (this.autoplay) {
+      this.player.play();
     }
   }
 
-  searchAndLoadVideo() {
-    return this.search().then((videos) => {
-      if (videos.length) {
-        let videoId = videos[0];
-        return this.loadVideo(videoId);
-      }
-      return new Promise.resolve(false);
-    });
+  onPlayerEnded() {
+    if (this.currentVideoIndex >= this.videos.length - 1) {
+      this.trigger("ended");
+    }
+    else {
+      this.loadNextVideo();
+    }
   }
 
-  loadVideo(videoId) {
+  fetchVideos() {
     if (!this.player) {
       return Promise.resolve(false);
     }
 
+    let query = null;
+    try {
+      query = "ref:dest_" + window.lp.place.atlasId;
+    }
+    catch (e) {
+      return Promise.resolve(false);
+    }
+
     return new Promise((resolve) => {
-      this.player.catalog.getVideo(videoId, (error, video) => {
+      this.player.catalog.getPlaylist(query, (error, playlist) => {
         if (!error) {
-          this.player.catalog.load(video);
-          this.renderSEOMarkup();
+          this.videos = playlist.length ? playlist : [];
         }
-        resolve(!error);
+        if (this.videos.length) {
+          this.loadNextVideo();
+          resolve(true);
+        }
+        else {
+          this.player.catalog.getVideo(query, (error, video) => {
+            if (!error) {
+              this.videos = [video];
+              this.loadNextVideo();
+            }
+            resolve(!error);
+          });
+        }
       });
     });
   }
 
-  /**
-   * Used to set the initial dimensions of the video player
-   * so that when video data begins to load, it sees that the player is fairly
-   * large and loads high-res video data.  We have an issue with Brightcove at the moment
-   * where it seems to load lower-res video if the player size is set to "mobile-like" 
-   * dimensions, but we want to make sure we always have high-res video loaded (if available).
-   *
-   * This is run when the player is initially setup so consider resizing
-   * this.videoEl before making the player visible.
-   */
-  setInitialDimensions() {
-    if (!this.player) {
+  loadNextVideo() {
+    if (!this.player || !this.videos.length) {
       return;
     }
 
-    let width = 1280;
-    let height = width / this.defaultAspectRatio;
+    this.currentVideoIndex = this.currentVideoIndex === null ? 0 : this.currentVideoIndex + 1;
+    this.currentVideoIndex = this.currentVideoIndex > this.videos.length - 1 ? 0 : this.currentVideoIndex;
 
-    this.player.dimensions(width, height);
+    this.player.catalog.load(this.videos[this.currentVideoIndex]);
   }
 
   /**
    * Gets the ideal dimensions of the video, considering it's aspect ratio.
    * @param {number} maxw - (required) Maximum width to return (pixels)
-   * @param {number} maxh - (optional) Maximum height to return (pixels) 
+   * @param {number} maxh - (optional) Maximum height to return (pixels)
    * @return {Object} object with 'width' and 'height' attributes
    */
   getIdealDimensions(maxw, maxh) {
     let ratio = this.defaultAspectRatio;
 
-    // If we have video data, use the aspect ratio of the 
+    // If we have video data, use the aspect ratio of the
     // video as the width-height ratio value
     try {
       let source = this.player.mediainfo.rawSources[0];
@@ -127,6 +147,27 @@ class Brightcove extends VideoPlayer {
     return { width: width, height: height };
   }
 
+  configureOverlays() {
+    if (!this.player) {
+      return;
+    }
+
+    const overlays = [{
+      content: "<div class=\"video__ad-overlay\">Advertisement</div>",
+      align: "top-left",
+      start: "ads-ad-started",
+      end: "playing",
+    }];
+
+    this.player.overlay({
+      content: "",
+      overlays,
+      showBackground: false,
+      attachToControlBar: true,
+      debug: false,
+    });
+  }
+
   /**
    * Retrieves metadata from the currently loaded video
    * @param {string} name - The metadata attribute to retrieve a value for
@@ -141,55 +182,7 @@ class Brightcove extends VideoPlayer {
   }
 
   /**
-   * Retrieves SEO-friendly metadata about the highest-resolution "source" 
-   * of the currently loaded video
-   *
-   * @returns {Object} object with 'url', 'width', and 'height' 
-   *   attributes, null if unable to retrieve valid metadata
-   */
-  getVideoSourceData () {
-
-    if (!this.player || !this.player.mediainfo) {
-      return null;
-    }
-
-    // Schema.org VideoObject valid types include:
-    //   .mpg, .mpeg, .mp4, .m4v, .mov, .wmv, .asf, .avi, .ra, .ram, .rm, .flv
-    //   (Some of these are skipped here because they pertain to audio-only file types)
-    //   https://developers.google.com/webmasters/videosearch/schema
-    let validTypes = [
-      "video/mpeg",
-      "video/mp4",
-      "video/x-m4v",
-      "video/quicktime",
-      "video/x-ms-wmv",
-      "video/x-ms-asf",
-      "video/x-msvideo",
-      "application/vnd.rn-realmedia",
-      "video/x-flv"
-    ];
-
-    let url = null;
-    let width = 0;
-    let height = 0;
-
-    $.each(this.player.mediainfo.sources, function (i, source) {
-      if (!source.type || validTypes.indexOf(source.type.toLowerCase()) == -1) {
-        return;
-      }
-
-      if (!url || (width < source.width)) {
-        url = source.src;
-        width = source.width;
-        height = source.height;
-      }
-    });
-
-    return url ? { url: url, width: width, height: height } : null;
-  }
-
-  /**
-   * Uses the currently loaded video data to build a block of 
+   * Uses the currently loaded video data to build a block of
    * LD-JSON Schema.org markup and appends it to the "head" tag
    *
    * This is run automatically when any video is loaded.
@@ -206,11 +199,6 @@ class Brightcove extends VideoPlayer {
       return;
     }
 
-    let src = this.getVideoSourceData();
-    if (!src) {
-      return;
-    }
-
     let defaultDescription = "";
     try {
       defaultDescription = window.lp.place.name;
@@ -224,7 +212,7 @@ class Brightcove extends VideoPlayer {
     let seconds = Math.ceil(this.getVideoProperty("duration"));
     let duration = "PT" + seconds + "S";
 
-    let embedUrl = "http://players.brightcove.net/5104226627001/default_default/index.html?videoId=" + videoId;
+    let embedUrl = "https://players.brightcove.net/5104226627001/default_default/index.html?videoId=" + videoId;
 
     let data = {
       "@context": "http://schema.org",
@@ -232,17 +220,9 @@ class Brightcove extends VideoPlayer {
       "name": this.getVideoProperty("name") || defaultDescription,
       "description": this.getVideoProperty("description") || defaultDescription,
       "thumbnailURL": this.getVideoProperty("thumbnail"),
-      "contentURL": src.url,
       "embedURL": embedUrl,
       "duration": duration,
       "uploadDate": this.getVideoProperty("createdAt"),
-
-      // Leaving width and height off because they are optional and the width and height returned from
-      // this.getVideoSourceData() isn't necessarily the largest dimensions the video can be rendered at.
-      // All source data in brightcove's video metadata uses the same value for "contentURL" above, so 
-      // Google, Bing, etc. should be able to determine the resolution of the video on their own.
-      // "height": src.height,
-      // "width": src.width,
     };
 
     script = document.createElement("script");
