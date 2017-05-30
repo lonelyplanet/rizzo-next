6c7d871040b858e8362a860b4f9286a0f0c2eed7
Tom Cunningham <tom.cunningham@lonelyplanet.com>
2017-05-30 12:21:30 -0500

    Default features key in ArticleModel to empty array

--- src/components/article/article_model.js
+++ src/components/article/article_model.js
@@ -3,7 +3,7 @@ import Model from "../../core/model";
 export default class ArticleModel extends Model {
   parse(response) {
     return Object.assign({}, response.article, {
-      features: response.features
+      features: response.features || [],
     });
   }
 };
