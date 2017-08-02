02e22e74e89d71a26b28930e276cd44597f415ef
Nick Franciosi <nickfranciosi@gmail.com>
2017-08-02 14:02:00 -0500

    fix: banner styles for 1%

--- src/components/beta_banner/beta_banner_component.js
+++ src/components/beta_banner/beta_banner_component.js
@@ -5,35 +5,5 @@ export default class BetaBannerComponent {
     this.$betaBannerTemplate = require("../beta_banner/beta_banner.hbs");
     this.$renderedBanner = $(this.$betaBannerTemplate())
       .prependTo("body");
-
-    this.bindToFeedbackButton();
-    this.bindToExitButton();
   }
-
-  bindToFeedbackButton() {
-    this.$renderedBanner
-    .find("#feedback-beta")
-    .on("click", (e) => {
-      e.preventDefault();
-      window.usabilla_live && window.usabilla_live("trigger", "profile feedback");
-    });
-  }
-
-  bindToExitButton() {
-    this.$renderedBanner
-    .find("#exit-beta")
-    .on("click", (e) => {
-      e.preventDefault();
-      this.leaveBeta();
-    });
-  }
-
-  leaveBeta() {
-    Cookie.remove("_v", { domain: ".lonelyplanet.com" });// Remove Variant
-    // give a little cushion before redirecting
-    setTimeout(() => {
-      window.location = "//connect.lonelyplanet.com/users/sign_out"; // Sign Out
-    }, 500);
-  }
-
 }
