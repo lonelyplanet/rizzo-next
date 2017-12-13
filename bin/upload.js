bec179ab74faa4c8d8436bc14f665564935be3fd
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2017-12-12 18:28:07 -0600

    improve upload script

--- bin/upload.js
+++ bin/upload.js
@@ -6,25 +6,24 @@ const path = require("path");
 
 const s3 = new AWS.S3();
 const s3Bucket = "static-asset";
-const key = `rizzo-next/${pkg.version}/`;
+const baseKey = "rizzo-next";
+const version = pkg.version;
 
-const upload = (filename) => new Promise((resolve, reject) => {
-  const filePath = path.join(process.cwd(), filename);
-  const basename = path.basename(filename);
-  const body = fs.readFileSync(filePath);
+const upload = (Key, Body) => new Promise((resolve, reject) => {
   const mimeTypes = {
     html: "text/html",
     js: "application/javascript",
     css: "text/css",
+    json: "application/json",
   };
-  const ext = filename.split(".")[1];
-  const Key = `${key}${basename}`;
+
+  const ext = path.extname(Key).replace(/^./, "");
 
   s3.putObject({
     Bucket: s3Bucket,
     Key,
     ACL: "public-read",
-    Body: body,
+    Body,
     ContentType: mimeTypes[ext],
     CacheControl: "public, max-age=0, s-maxage=86400",
   }, (err) => {
@@ -36,9 +35,25 @@ const upload = (filename) => new Promise((resolve, reject) => {
   });
 });
 
+const componentsKey = `${baseKey}/components.json`;
 const files = glob.sync("dist/*.*");
 
-Promise.all(files.map(upload))
+const promises = files.map((filename) => {
+  const filePath = path.join(process.cwd(), filename);
+  const basename = path.basename(filename);
+  const body = fs.readFileSync(filePath);
+  const key = `${baseKey}/${version}/${basename}`;
+
+  return upload(key, body);
+});
+Promise.all([
+  ...promises,
+  upload(componentsKey, JSON.stringify({
+    header: `${baseKey}/${version}/header.html`,
+    footer: `${baseKey}/${version}/footer.html`,
+    version,
+  })),
+])
 .then((results) => {
   console.log(`Uploaded ${results.length} files to s3.`);
 });
