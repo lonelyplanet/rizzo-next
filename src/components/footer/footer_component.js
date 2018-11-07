3ebeb9762fa3ece18a1226abf949faa3315ab6f8
johnwquarles <john.w.quarles@gmail.com>
2018-11-07 11:50:30 -0600

    feat: track newsletter subscriptions in modal and footer

--- src/components/footer/footer_component.js
+++ src/components/footer/footer_component.js
@@ -1,8 +1,10 @@
 import { Component } from "../../core/bane";
+import { analytics, EventNames, getTrackMethod } from "@lonelyplanet/lp-analytics";
 import $ from "jquery";
 
 class Footer extends Component {
   initialize() {
+    this.track = getTrackMethod();
     this.updateLocationOnChange();
     this.$form = $(".js-newsletter-form");
     this.$form.on("submit", this.submit.bind(this));
@@ -22,6 +24,9 @@ class Footer extends Component {
   handleSubmitSuccess() {
     this.$form.addClass("is-hidden");
     this.$success.removeClass("is-hidden");
+    this.track({
+      [analytics.eventName]: EventNames.newsletterSubscription,
+    });
   }
 
   submit(event) {
