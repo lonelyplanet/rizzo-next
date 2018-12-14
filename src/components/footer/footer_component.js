7e391a4500ac97d90bb5a5c53df8e007206739cb
johnwquarles <john.w.quarles@gmail.com>
2018-12-14 16:25:53 -0600

    chore: excise lp-analytics

--- src/components/footer/footer_component.js
+++ src/components/footer/footer_component.js
@@ -1,10 +1,8 @@
 import { Component } from "../../core/bane";
-import { analytics, EventNames, getTrackMethod } from "@lonelyplanet/lp-analytics";
 import $ from "jquery";
 
 class Footer extends Component {
   initialize() {
-    this.track = getTrackMethod();
     this.updateLocationOnChange();
     this.$form = $(".js-newsletter-form");
     this.$form.on("submit", this.submit.bind(this));
@@ -24,9 +22,10 @@ class Footer extends Component {
   handleSubmitSuccess() {
     this.$form.addClass("is-hidden");
     this.$success.removeClass("is-hidden");
-    this.track({
-      [analytics.eventName]: EventNames.newsletterSubscription,
-    });
+    if (typeof window !== "undefined" && window.dataLayer && Array.isArray(window.dataLayer)) {
+      // shim for lp-analytics; importing the module seems to break rizzo (not this, rizzo-next, but legacy rizzo, which imports this)
+      window.dataLayer.push({ event: "newsletter-subscribe" });
+    }
   }
 
   submit(event) {
