deeb4817a19bf58bea4ec1b1d2347fb2e0694fad
vacas5 <russelljanderson@gmail.com>
2019-10-15 09:40:20 -0500

    fix: clean dfp strings of special characters
    Google ads are very particular about which special characters are
    allowed and which are not. Many of our place names contain special latin
    characters, commas or parentheses. Clean these before slugifying and
    passing to DFP as key values. This will fix an issue with two places in
    the upcoming BiT campaign that should be targeted.

--- src/core/utils/stringHelpers.js
+++ src/core/utils/stringHelpers.js
@@ -1,14 +1,20 @@
+import deburr from "lodash/deburr";
 /**
  * Takes a given string and turns it into a hyphenated slug
  * @param  {String} string String to replace
  * @return {String}
  */
-let slugify = (string) => {
+const clean = string => {
+  // allow only basic latin alpha, numeric, / - _ and whitespace
+  return deburr(string).replace(/[^/-\w\s]/gi, "");
+};
+
+let slugify = string => {
   if (!string || typeof string !== "string") return "";
 
-  return string.toLowerCase().replace(/\s/g, "-");
+  return clean(string)
+    .toLowerCase()
+    .replace(/\s/g, "-");
 };
 
-export {
-  slugify
-};
+export { slugify };
